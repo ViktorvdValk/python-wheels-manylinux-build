03aea3d0e34456ed57dd1e1502ce55e7be87ee0f
ViktorvdV <ViktorvdValk>
2021-01-12 10:30:17 +0100

    'Adjusted_to_use_with_make/cmake'

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,24 +4,24 @@ set -e -x
 # CLI arguments
 PY_VERSIONS=$1
 BUILD_REQUIREMENTS=$2
-SYSTEM_PACKAGES=$3
-PRE_BUILD_COMMAND=$4
-PACKAGE_PATH=$5
-PIP_WHEEL_ARGS=$6
+# SYSTEM_PACKAGES=$3
+# PRE_BUILD_COMMAND=$4
+PACKAGE_PATH=$3
+# PIP_WHEEL_ARGS=$6
 
 # Temporary workaround for LD_LIBRARY_PATH issue. See
 # https://github.com/RalfG/python-wheels-manylinux-build/issues/26
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
 
 cd /github/workspace/"${PACKAGE_PATH}"
-
-if [ ! -z "$SYSTEM_PACKAGES" ]; then
-    yum install -y ${SYSTEM_PACKAGES}  || { echo "Installing yum package(s) failed."; exit 1; }
-fi
-
-if [ ! -z "$PRE_BUILD_COMMAND" ]; then
-    $PRE_BUILD_COMMAND || { echo "Pre-build command failed."; exit 1; }
-fi
+#
+# if [ ! -z "$SYSTEM_PACKAGES" ]; then
+#     yum install -y ${SYSTEM_PACKAGES}  || { echo "Installing yum package(s) failed."; exit 1; }
+# fi
+#
+# if [ ! -z "$PRE_BUILD_COMMAND" ]; then
+#     $PRE_BUILD_COMMAND || { echo "Pre-build command failed."; exit 1; }
+# fi
 
 # Compile wheels
 arrPY_VERSIONS=(${PY_VERSIONS// / })
@@ -35,7 +35,13 @@ for PY_VER in "${arrPY_VERSIONS[@]}"; do
     fi
 
     # Build wheels
-    /opt/python/"${PY_VER}"/bin/pip wheel . ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
+    cd ..
+    mkdir build
+    cd build
+    cmake ../SimpleElastix/SuperBuild
+    make -j4
+    make -C SimpleITK-build dist
+    # /opt/python/"${PY_VER}"/bin/pip wheel . ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
 done
 
 # Bundle external shared libraries into the wheels
